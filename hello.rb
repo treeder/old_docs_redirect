5aeba55eb0b51b1033b6e47c983c0bd2f100c1f4
Travis Reeder <treeder@gmail.com>
2012-03-05 14:58:25 -0800

    Set redirect on * to dev.iron.io

--- hello.rb
+++ hello.rb
@@ -2,19 +2,7 @@ require 'sinatra'
 
 set :public_folder, File.dirname(__FILE__) + '/static'
 
-
-get '/' do
-  if request.host == "blog.simpleworker.com"
-    redirect "#{request.scheme}://blog.iron.io#{request.path}"
-    return true
-  end
-  erb :hello
-end
-
 get '*' do
-  if request.host == "blog.simpleworker.com"
-    redirect "#{request.scheme}://blog.iron.io#{request.path}"
-    return true
-  end
-  redirect '/'
+  redirect "http://dev.iron.io"
 end
+
